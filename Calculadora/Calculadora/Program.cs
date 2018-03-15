985626f37f2f99e72974f6c020a42232dee74eef
Eduardo Pedroso <eduardo_pedroso95@hotmail.com>
2018-03-15 14:44:47 +0000

    MyFirstProjet

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            calculator.Start();
+        }
+
+    }
+}
